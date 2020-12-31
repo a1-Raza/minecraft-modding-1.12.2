5e524f0e83ac6ab72c78fde0a8895ed2500e43ef
Abdurrahman Raza <abdurrahmanraza10@gmail.com>
2020-12-30 17:41:54 -0800

    WIP10.0: created WorldGenEndMinable class in attempt to get ender ore to generate in the end

--- src/main/java/X_Blocks1/firstmod/world/ModWorldGen.java
+++ src/main/java/X_Blocks1/firstmod/world/ModWorldGen.java
@@ -3,6 +3,7 @@ package X_Blocks1.firstmod.world;
 import java.util.Random;
 
 import X_Blocks1.firstmod.init.ModBlocks;
+import X_Blocks1.firstmod.world.WorldGenEndMinable;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -18,13 +19,20 @@ public class ModWorldGen implements IWorldGenerator
 	{
 		if (world.provider.getDimension() == 0)
 		{
-			generateOverworld(random, chunkX, chunkZ, world, chunkGenerator, chunkProvider);
+			//generateOverworld(random, chunkX, chunkZ, world, chunkGenerator, chunkProvider);
+			generateEnd(random, chunkX, chunkZ, world, chunkGenerator, chunkProvider);
 		}
 	}
 	
 	private void generateOverworld(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider)
 	{
 		generateOre(ModBlocks.ENDER_ORE.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 5, 15, random.nextInt(4) + 1, 4);
+	}
+	
+	//NEW
+	private void generateEnd(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider)
+	{
+		generateEndOre(ModBlocks.ENDER_ORE.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 20, 80, random.nextInt(4) + 1, 100);
 	}
 	
 	
@@ -40,4 +48,20 @@ public class ModWorldGen implements IWorldGenerator
 			generator.generate(world, random, pos);
 		}
 	}
+	
+	//NEW
+	private void generateEndOre(IBlockState ore, World world, Random random, int x, int z, int minY, int maxY, int size, int chances)
+	{
+		int deltaY = maxY - minY;
+		
+		for(int i = 0; i < chances; i++)
+		{
+			BlockPos pos = new BlockPos(x + random.nextInt(16), minY + random.nextInt(deltaY), z + random.nextInt(16));
+			
+			WorldGenEndMinable generator = new WorldGenEndMinable(ore, size);
+			generator.generate(world, random, pos);
+			
+		}
+	}
+	
 }
