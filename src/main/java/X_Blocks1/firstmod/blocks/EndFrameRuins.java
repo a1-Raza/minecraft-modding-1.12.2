002b337c5c5c5a22984562bcb7b61c4d0edd7fa8
Abdurrahman Raza <abdurrahmanraza10@gmail.com>
2021-01-23 19:26:58 -0800

    WIP11 (Completed): got end frame ruins generation working, also changed blocks properties to be directional block (created new class for that called BlockDirectional)

--- src/main/java/X_Blocks1/firstmod/blocks/EndFrameRuins.java
+++ src/main/java/X_Blocks1/firstmod/blocks/EndFrameRuins.java
@@ -25,29 +25,21 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
-public class EndFrameRuins extends BlockHorizontal implements IHasModel
+public class EndFrameRuins extends BlockDirectional
 {
-	
-	public static final PropertyDirection FACING = BlockHorizontal.FACING;
 	public static final AxisAlignedBB END_FRAME_RUINS_AABB = new AxisAlignedBB(0, 0, 0, 1, 0.8125D, 1);
 	
 	// Block General Properties
 	public EndFrameRuins(String name, Material material) 
 	{
-		super(material);
+		super(name, material);
 		
-		setUnlocalizedName(name);
-		setRegistryName(name);
 		setCreativeTab(CreativeTabs.DECORATIONS);
-		
-		ModBlocks.BLOCKS.add(this);
-		ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
 		
 		setSoundType(SoundType.GLASS);
 		setHardness(5.0F);
 		setResistance(100.0F);
 		setHarvestLevel("pickaxe", 3);
-		setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
 	}
 	
 	// Block Hitbox and Texture
@@ -68,38 +60,4 @@ public class EndFrameRuins extends BlockHorizontal implements IHasModel
 	{
 		return END_FRAME_RUINS_AABB;
 	}
-	
-	// Block Direction
-	@Override
-	public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
-    {
-        return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
-    }
-    
-	@Override
-	public int getMetaFromState(IBlockState state)
-    {
-        int i = 0;
-        i = i | ((EnumFacing)state.getValue(FACING)).getHorizontalIndex();
-        return i;
-	}
-	
-	@Override
-	public IBlockState getStateFromMeta(int meta)
-    {
-        return this.getDefaultState().withProperty(FACING, EnumFacing.getHorizontal(meta & 3));
-    }
-	
-	protected BlockStateContainer createBlockState()
-    {
-        return new BlockStateContainer(this, new IProperty[] {FACING});
-    }
-	
-	// Block Rendering
-	@Override
-	public void registerModels() 
-	{
-		Main.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
-	}
-	
 }
